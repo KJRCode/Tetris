8bea594e6ce84726591b41a253a5f3efe6496052
RIPPERKJ22 <RipperKJ22@gcc.edu>
2023-05-04 10:08:46 -0400

    started a OneBlock piece

---
+++ src/main/java/com/example/welovetetris/OneBlock.java
@@ -0,0 +1,13 @@
+package com.example.welovetetris;
+
+public class OneBlock extends Pieces{
+    public OneBlock() {
+        super();
+        super.pChar = '1';
+        super.p = new boolean[4][4];
+    }
+
+    public void pieceShape() {
+        //for (int i = 0; i < )
+    }
+}
--- src/main/java/com/example/welovetetris/Pieces.java
+++ src/main/java/com/example/welovetetris/Pieces.java
@@ -1,6 +1,11 @@
 package com.example.welovetetris;
 
 public abstract class Pieces {
+    protected boolean[][] p;
+    protected char pChar;
+    public Pieces(){
+
+    }
     public void turnPieceClockwise() {
 
     }
