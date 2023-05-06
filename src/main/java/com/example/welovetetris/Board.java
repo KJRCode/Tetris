222b82a31f604f5d48dc56deea51d2da87456428
winona-w <wherleyws21@gcc.edu>
2023-05-06 18:27:28 -0400

    i hard coded it so that the moveDown method works for a three by one shape/line

--- src/main/java/com/example/welovetetris/Board.java
+++ src/main/java/com/example/welovetetris/Board.java
@@ -47,25 +47,28 @@ public class Board {
         boolean[][] temp = new boolean[NUM_ROWS][NUM_COLS];
 
         for (int i = 0; i < NUM_COLS; i++) {
-            if (!board[rowNum][i]) {
+            if (!board[rowNum][i] && !currentBoard[rowNum][i]) {
                 return;
             }
         }
 
         for (int i = 0; i < NUM_COLS; i++) {
             board[rowNum][i] = false;
+            currentBoard[rowNum][i] = false;
         }
         numLinesCleared++;
 
         for (int i = rowNum+1; i >= 2; i--) {
             for (int j = 0; j < NUM_COLS; j++) {
                 temp[i-1][j] = board[i-2][j];
+                temp[i-1][j] = currentBoard[i-2][j];
             }
         }
 
         for (int i = rowNum; i >= 0; i--) {
             for (int j = 0; j < NUM_COLS; j++) {
                 board[i][j] = temp[i][j];
+                currentBoard[i][j] = temp[i][j];
             }
         }
     }
@@ -81,13 +84,26 @@ public class Board {
 
     public void moveDown() {
         if (y < NUM_ROWS-1) {
-            if (!board[y+1][x]) {
+            if (!board[y+1][x] && !currentBoard[y+1][x]
+            && !board[y+1][x+1] && !currentBoard[y+1][x+2]
+            && !board[y+1][x+2] && !currentBoard[y+1][x+2]) {
+                //pieceChange(currentBoard, 1, 0);
                 currentBoard[y][x] = false;
+                currentBoard[y][x+1] = false;
+                currentBoard[y][x+2] =  false;
                 y += 1;
                 currentBoard[y][x] = true;
+                currentBoard[y][x+1] = true;
+                currentBoard[y][x+2] =  true;
+
             } else {
                 pieceLanded = true;
                 board[y][x] = true;
+                board[y][x+1] = true;
+                board[y][x+2] =  true;
+                currentBoard[y][x] = true;
+                currentBoard[y][x+1] = true;
+                currentBoard[y][x+2] =  true;
                 x = 10;
                 y = 0;
             }
@@ -95,6 +111,11 @@ public class Board {
         else {
             pieceLanded = true;
             board[y][x] = true;
+            board[y][x+1] = true;
+            board[y][x+2] =  true;
+            currentBoard[y][x] = true;
+            currentBoard[y][x+1] = true;
+            currentBoard[y][x+2] =  true;
             x = 10;
             y = 0;
         }
@@ -102,18 +123,42 @@ public class Board {
     public void moveLeft() {
         if (x > 0) {
             if(!board[y-1][x-1]) {
+                //pieceChange(currentBoard, 0, -1);
+                currentBoard[y][x] = false;
+                currentBoard[y][x+1] = false;
+                currentBoard[y][x+2] =  false;
+                x -= 1;
+                currentBoard[y][x] = true;
+                currentBoard[y][x+1] = true;
+                currentBoard[y][x+2] =  true;
+
+                /*
                 currentBoard[y][x] = false;
                 x -= 1;
                 currentBoard[y][x] = true;
+
+                 */
             }
         }
     }
     public void moveRight() {
         if (x < NUM_COLS-1-2) { //-2 is number of squares after the first one
-            if(!board[y][x+1]) {
+            if(!board[y][x+1+2]) {
+                //pieceChange(currentBoard, 0, 1);
+                currentBoard[y][x] = false;
+                currentBoard[y][x+1] = false;
+                currentBoard[y][x+2] =  false;
+                x += 1;
+                currentBoard[y][x] = true;
+                currentBoard[y][x+1] = true;
+                currentBoard[y][x+2] =  true;
+
+                /*
                 currentBoard[y][x] = false;
                 x += 1;
                 currentBoard[y][x] = true;
+
+                 */
             }
         }
     }
@@ -134,18 +179,24 @@ public class Board {
             // fill in this row
             for (int c = 0; c < NUM_COLS; c++) {
                 //adding landed pieces
-                frame.append(' ');
+                //frame.append(' ');
 
-                if (board[r][c]) {//we stop in the upper left corner and draw the whole piece
-                    addLandedPieces(frame);
+                if (board[r][c] || currentBoard[r][c]) {//we stop in the upper left corner and draw the whole piece
+                    frame.append("C");
+                    //addLandedPieces(frame);
                 } else {
+                    frame.append(' ');
+                    /*
                     //moving piece currently being played with
                     if (r == y && c == x) {
-                        addCurrentPiece(frame);
+                        frame.append("C");
+                        //addCurrentPiece(frame);
                     }
                     else if (!currentBoard[r][c]){
                         //frame.append(' ');
-                    }
+
+
+                    }*/
                 }
             }
             // add a right border
@@ -177,5 +228,22 @@ public class Board {
                 }
             }
         }
+    }
+
+    public void pieceChange(boolean[][] b, int dY, int dX) {
+        for (int i = 0; i < NUM_ROWS; i++) {
+            for (int j = 0; j < NUM_COLS; j++) {
+                b[i][j] = false;
+                if ((i + dY < 20) && (j + dX < 20) && (i + dY >= 0) && (j + dX >= 0)) {
+                    b[i + dY][j + dX] = currentBoard[i][j];
+                }
+            }
+        }
+        for (int i = 0; i < NUM_ROWS; i++) {
+            for (int j = 0; j < NUM_COLS; j++) {
+                currentBoard[i][j] = b[i][j];
+            }
+        }
+
     }
 }
